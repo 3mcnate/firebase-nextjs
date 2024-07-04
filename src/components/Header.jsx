b7e9089a60564863fe6892ad13d40f299073bb52
Nate Boxer <nboxer@usc.edu>
2024-07-03 23:25:07 -0700

    auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,64 @@
-'use client'
-import React, { useState, useEffect } from "react";
-import Link from "next/link";
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import {
 	signInWithGoogle,
 	signOut,
-	onAuthStateChanged
-} from "@/src/lib/firebase/auth.js";
-import { addFakeRestaurantsAndReviews } from "@/src/lib/firebase/firestore.js";
-import { useRouter } from "next/navigation";
-import { firebaseConfig } from "@/src/lib/firebase/config";
+	onAuthStateChanged,
+} from '@/src/lib/firebase/auth.js';
+import { addFakeRestaurantsAndReviews } from '@/src/lib/firebase/firestore.js';
+import { useRouter } from 'next/navigation';
+import { firebaseConfig } from '@/src/lib/firebase/config';
 
 function useUserSession(initialUser) {
-	return;
+	// initialUser comes from the server via a server component
+	const [user, setUser] = useState(initialUser);
+	const router = useRouter();
+
+	// register the service worker that sends auth state back to server
+	// The service worker is built with npm run build-service worker
+	useEffect(() => {
+		if ('serviceWorker' in navigator) {
+			const serializedFirebaseConfig = encodeURIComponent(
+				JSON.stringify(firebaseConfig),
+			);
+			const serviceWorkerUrl = `/auth-service-worker.js?firebaseConfig=${serializedFirebaseConfig}`;
+
+			navigator.serviceWorker
+				.register(serviceWorkerUrl)
+				.then((registration) => console.log('scope is: ', registration.scope));
+		}
+	}, []);
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged((authUser) => {
+			setUser(authUser);
+		});
+
+		return () => unsubscribe();
+	}, []);
+
+	useEffect(() => {
+		onAuthStateChanged((authUser) => {
+			if (user === undefined) return;
+
+			if (user?.email !== authUser?.email) {
+				router.refresh();
+			}
+		}, [user]);
+	});
 }
 
-export default function Header({initialUser}) {
+export default function Header({ initialUser }) {
+	const user = useUserSession(initialUser);
 
-	const user = useUserSession(initialUser) ;
-
-	const handleSignOut = event => {
+	const handleSignOut = (event) => {
 		event.preventDefault();
 		signOut();
 	};
 
-	const handleSignIn = event => {
+	const handleSignIn = (event) => {
 		event.preventDefault();
 		signInWithGoogle();
 	};
@@ -38,7 +73,11 @@ export default function Header({initialUser}) {
 				<>
 					<div className="profile">
 						<p>
-							<img className="profileImage" src={user.photoURL || "/profile.svg"} alt={user.email} />
+							<img
+								className="profileImage"
+								src={user.photoURL || '/profile.svg'}
+								alt={user.email}
+							/>
 							{user.displayName}
 						</p>
 
@@ -48,10 +87,7 @@ export default function Header({initialUser}) {
 								<li>{user.displayName}</li>
 
 								<li>
-									<a
-										href="#"
-										onClick={addFakeRestaurantsAndReviews}
-									>
+									<a href="#" onClick={addFakeRestaurantsAndReviews}>
 										Add sample restaurants
 									</a>
 								</li>
@@ -66,10 +102,12 @@ export default function Header({initialUser}) {
 					</div>
 				</>
 			) : (
-				<div className="profile"><a href="#" onClick={handleSignIn}>
-					<img src="/profile.svg" alt="A placeholder user image" />
-					Sign In with Google
-				</a></div>
+				<div className="profile">
+					<a href="#" onClick={handleSignIn}>
+						<img src="/profile.svg" alt="A placeholder user image" />
+						Sign In with Google
+					</a>
+				</div>
 			)}
 		</header>
 	);
